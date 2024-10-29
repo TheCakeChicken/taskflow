2b73f986a4972b3bbf42b64c92f23ad80a6bdddd
Alyx <cake@cakechicken.me>
2024-10-29 20:06:46 +0000

    themes :eyes:

--- core.js
+++ core.js
@@ -1,4 +1,5 @@
 const linkTemplate = "https://{instance}.simprocloud.com/staff/taskDetails.php?taskID={id}&iframe=true"
+const THEMES = ['98', 'xp', '7']
 
 $.fn.enterKey = function (fnc) {
     return this.each(function () {
@@ -16,6 +17,9 @@ $(() => {
         makeWebsiteGoNow();
     })
 
+    const theme = localStorage.getItem('theme');
+    setTheme(theme || THEMES[0]);
+
     const inst = localStorage.getItem('instance');
     if (inst) {
         $('#instance-input').val(inst);
@@ -28,6 +32,15 @@ function close() {
 
 function clearTextbox() {
     $('#id-input').val('');
+}
+
+function setTheme(theme) {
+    if (!THEMES.includes(theme)) return;
+
+    $('#core-css').attr('href', `https://unpkg.com/${theme}.css`);
+    $('html').attr('data-theme', theme);
+
+    localStorage.setItem('theme', theme);
 }
 
 function makeWebsiteGoNow() {
